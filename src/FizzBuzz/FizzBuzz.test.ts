daba8a70365811cbdcac333fdd9a786c7fff16da
Taiki Ohara <taikiohara@example.com>
2021-08-21 00:33:24 +0900

    デモ用ブランチの準備

--- src/FizzBuzz/FizzBuzz.test.ts
+++ src/FizzBuzz/FizzBuzz.test.ts
@@ -1,16 +1,1 @@
-import { fizzBuzzMachine } from "./FizzBuzz"
-
-describe("FizzBuzz問題のテスト", () => {
-  it("数値を入力すると、文字列の数値を返す_1 を入力すると、文字列 1 を返すことを確認", () => {
-    expect(fizzBuzzMachine(1)).toBe("1")
-  })
-  it("3 の倍数を入力すると、Fizz を返す_3 を入力すると、Fizzを返すことを確認", () => {
-    expect(fizzBuzzMachine(3)).toBe("Fizz")
-  })
-  it("5 の倍数を入力すると、Buzz を返す_100 を入力すると、Buzzを返すことを確認", () => {
-    expect(fizzBuzzMachine(100)).toBe("Buzz")
-  })
-  it("3と5 の倍数を入力すると、FizzBuzz を返す_15 を入力すると、FizzBuzzを返すことを確認", () => {
-    expect(fizzBuzzMachine(15)).toBe("FizzBuzz")
-  })
-})
+// TODO テスト書く
--- src/FizzBuzz/FizzBuzz.ts
+++ src/FizzBuzz/FizzBuzz.ts
@@ -1,6 +1,1 @@
-export const fizzBuzzMachine = (num: number): string => {
-  if (num % 15 === 0) return "FizzBuzz"
-  if (num % 3 === 0) return "Fizz"
-  if (num % 5 === 0) return "Buzz"
-  return num.toString()
-}
+// TODO 実装する
